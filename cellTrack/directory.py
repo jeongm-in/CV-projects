0b6c9ee929877b2046833dccd57d2bcf1b4f01c4
Jeong Min Lim <limjeongmin@wustl.edu>
2018-06-01 16:12:03 +0800

    Update and rename directory.py to preset.py
    Preset file will handle many experiment-specific parameters.

--- cellTrack/directory.py
+++
@@ -1,2 +0,0 @@
-fdirectory = 'full/directory/to/save/files'
-svideo = 'RT4_120_5' + '.avi'
---
+++ cellTrack/preset.py
@@ -0,0 +1,10 @@
+# File directory
+source_directory = r"C:\Users\jmlim\Documents\OpenCV\cellTrack\source\cuts\cd" + '\\'
+source_video = 'RT4_120_5' + '.avi'
+
+# Frequency Range
+begin_freq = 10000
+end_freq = 35000
+
+# Scale of pixel to um
+scale = 25
